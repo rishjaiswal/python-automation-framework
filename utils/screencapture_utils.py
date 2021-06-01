54f4848ca167833ea3a6bd7eef500da9ca934f16
Rishabh Jaiswal <rshbh.jaiswal@gmail.com>
2021-06-01 22:03:53 +0530

    Automation Framework Part-III ScreenshotUtils

--- utils/screencapture_utils.py
+++ utils/screencapture_utils.py
@@ -8,10 +8,10 @@ driver = DriverUtils.initialize_driver()
 
 
 class ScreenCaptureUtils:
-    log.debug("Implementation of ScreenCaptureUtils")
+    log.info("Implementation of ScreenCaptureUtils")
 
     def take_screenshot(test_case):
         # Python’s PIL library which lets us perform image operations= from PIL import Image
-        log.debug("Inside method take_screenshot() for capturing screenshots")
+        log.info("Inside method take_screenshot() for capturing screenshots")
         driver.save_screenshot(os.path.join(config.SCREENSHOT_PATH, test_case + ".png"))
-        log.debug("Finished method take_screenshot()")
+        log.info("Finished method take_screenshot()")
