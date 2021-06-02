e526293e573cd2a8f2ff64ba85f2b870b80ceb36
Rishabh Jaiswal <38694449+rishjaiswal@users.noreply.github.com>
2021-06-02 23:09:57 +0530

    Framework Part-IV ExcelUtils- Read TestData from Excel
    PR for Framework Part-IV
    
    ExcelUtils- Read TestData from Excel

--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -1,7 +1,24 @@
 from utils.logger_utils import get_logger
+from configs import config
+import openpyxl
 
 log = get_logger()
 
 
 class ExcelUtils:
-    print("TO-DO Implementation of ExcelUtils")
+    log.info("Implementation of ExcelUtils")
+
+    def read_from_excel():
+        log.info("Inside read_from_excel()")
+        complete_test_data = []
+        wb = openpyxl.load_workbook(config.TESTDATA_PATH)
+        sheet = wb.active
+        
+        # To get the data after headers of Excel as 1st row will be column names for data
+        for row in range(2, sheet.max_row + 1):
+            test_data = []
+            for column in range(1, sheet.max_column):
+                test_data.append(sheet.cell(row, column).value)
+            complete_test_data.append(test_data)
+        log.info("Finished read_from_excel()")
+        return complete_test_data
